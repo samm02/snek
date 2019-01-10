d1758075c76823162fea136f1df970609e5b6650
Sam Push <samm02@gmail.com>
2019-01-10 11:58:08 +0800

    first

---
+++ snake_game1.py
@@ -0,0 +1,15 @@
+
+import turtle
+
+# Set up the screen
+
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0) # turns off animantion on the screen
+
+# main code goes here
+
+
+wn.mainloop() # keeps the window open
