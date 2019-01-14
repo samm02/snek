ee13963982c8f8bee16a621b84b321196478af99
Sam Push <samm02@gmail.com>
2019-01-14 17:02:49 +0800

    name change

--- snake_game1.py
+++ snake_game1.py
@@ -12,7 +12,7 @@ high_score = 0
 # Set up the screen
 
 wn = turtle.Screen()
-wn.title("SNEK")
+wn.title("snek")
 wn.bgcolor("white")
 wn.setup(width=600, height=600)
 wn.tracer(0) # turns off animantion on the screen
